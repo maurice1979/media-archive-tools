2a2861b9b73e665718bf5c949edb4f8df41574a6
Jordi Vidal de Llobatera <is.maurice@protonmail.com>
2025-12-25 18:04:52 +0100

    chore: rename, add pre-commit

---
+++ src/media_archive/__init__.py
@@ -0,0 +1,1 @@
+"""Media archive tools."""
---
+++ src/media_archive/config.py
@@ -0,0 +1,15 @@
+"""Configuration variables."""
+
+from pathlib import Path
+
+nas_photo = Path("/Volumes/photo")
+
+SOURCE_DIR = Path("/Volumes/photo/movil_maurice")
+DEST_ROOT = nas_photo
+
+# Supported extensions
+PHOTO_EXTS = {".jpg", ".jpeg", ".png", ".heic", ".tiff"}
+VIDEO_EXTS = {".mp4", ".mov", ".avi", ".mkv"}
+
+ROOT_DIR = Path("/Volumes/photo")
+DRY_RUN = True  # ⚠️ SET TO False TO ACTUALLY DELETE FILES
